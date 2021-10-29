6350f98b467ffe75dfd5efde1b09da03882d5866
xan <47989552+style-x@users.noreply.github.com>
2021-10-29 20:10:12 +0200

    it works !
    now the lvl and dis/able button works :)

--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -5,37 +5,24 @@ function Clicker(props) {
   const [counter, setCounter] = useState(0);
   const hoch = () => {
     setCounter(counter + level);
-    proofdisabled();
   };
 
   const [level, levelup] = useState(1);
   const lvlup = () => {
     levelup(level + 1);
-    proofdisabled();
   };
 
-  const [lvldisabled, setlvldisabled] = useState(true);
-
-  function proofdisabled() {
-    if ((counter + level) >= (level * 10)) { 
-      setlvldisabled(false);
-    } else {
-      setlvldisabled(true);
-    };
-  };
-  
   function EnoughCoins () {
-    if ((counter + level) >= (level * 10)) {
+    if (counter >= (level * 10)) {
       lvlup();
       let count = level * 10;
       setCounter(counter - count);
       console.log("LVL UP", count);
-      proofdisabled();
     } else {
       console.log("Nicht genug Coins ?!");
     };
   };
-
+  
   return (
     <div className="Clicker">
         <h4>
@@ -43,10 +30,10 @@ function Clicker(props) {
         </h4>
         <p>Klicks: {counter} - Level: {level}</p>
         <button onClick={hoch} class="btn">Klick</button>
-        { lvldisabled ? (
-          <button onClick={EnoughCoins} className="btn btn-lvl disabled">{counter}</button>
+        { (counter >= (level * 10)) ? (
+          <button onClick={EnoughCoins} className="btn btn-lvl">LVL</button>
           ) : (
-          <button onClick={EnoughCoins} className="btn btn-lvl">LvL</button>
+          <button onClick={EnoughCoins} className="btn btn-lvl disabled">LVL</button>
         )} 
         <button className="btn btn-auto disabled">Auto</button>
     </div>
